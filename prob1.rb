87dc2f0f401c28de364bcd2b2062edc56cbf01f1
CawWoo <melodicraven@gmail.com>
2024-03-11 13:00:11 -0500

    Implemented custom getter and setter for second chance submission

--- prob1.rb
+++ prob1.rb
@@ -7,11 +7,24 @@ class Laptop
     @model = model
   end
 
+  def brand  new_brand
+    @brand = new_brand
+  end
+
+  def model new_model
+    @model = new_model
+  end
+
   def computer
     puts "#{@brand} #{@model}"
   end
 end
 
+puts "Original:"
 laptop = Laptop.new ("Dell"), ("XPS 13")
+laptop.computer
 
+puts
+puts "Update using setter:"
+laptop.model "XPS 15"
 laptop.computer
